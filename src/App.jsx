4d60913e36fe7f9756c92f202a437e8ddacadfb7
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-08-27 11:28:52 -0700

    Added Material UI, and started working on Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Nav from '../src/components/Nav';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Nav/> 
 <h1>First solo project!</h1>
     </>
   )
 }
 
-export default App
+export default App;
