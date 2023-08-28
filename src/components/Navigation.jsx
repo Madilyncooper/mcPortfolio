7cbc5a2f9178e98c12902182f147b06893d6fce0
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-08-27 19:59:24 -0700

    Adding styling to Navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,42 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation() {
-  return (
-    <>
-      <Navbar className='fixed-top' bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand className='fs-4' href="#home">Madilyn Cooper</Navbar.Brand>
-          <Nav className="me-auto p-4">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  );
+    return (
+        <>
+            <Navbar className='fixed-top p-4' bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand className='fs-2' href="#home">Madilyn Cooper</Navbar.Brand>
+                    <Nav className="me-auto p-4 navList fs-5">
+                        <Nav.Link className='about' href="#home">About</Nav.Link>
+                        <Navbar.Collapse id="navbar-dark-example">
+                            <Nav >
+                                <NavDropdown
+                               className='projects'
+                                    id="nav-dropdown-dark-example"
+                                    title="Projects"
+                                    menuVariant="dark"
+                                >
+                                    <NavDropdown.Item href="#action/3.1">MoodRing</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">
+                                        LeftOvers
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Garagio</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="https://github.com/Madilyncooper">
+                                        GitHub
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                        <Nav.Link href="#pricing">Resume</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
 
 export default Navigation;
