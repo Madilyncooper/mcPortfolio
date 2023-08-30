10062dba876ffcb6fa4dadfddeed2eb670ae0f79
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-08-29 18:12:01 -0700

    Adding styling to header/Navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ function App() {
 
   return (
     <>
+    <div className='bgImage'>
     <Navigation />
+    </div>
     </>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,11 +6,11 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function Navigation() {
     return (
         <>
-            <Navbar className='fixed-top p-4' bg="dark" data-bs-theme="dark">
+            <Navbar className='fixed-top p-1' bg="light" data-bs-theme="light">
                 <Container>
-                    <Navbar.Brand className='fs-2' href="#home">Madilyn Cooper</Navbar.Brand>
+                    <Navbar.Brand className='fs-3 name' href="#home">Madilyn Cooper</Navbar.Brand>
                     <Nav className="me-auto p-4 navList fs-5">
-                        <Nav.Link className='about' href="#home">About</Nav.Link>
+                        <Nav.Link target='blank' className='about' href="#home">About</Nav.Link>
                         <Navbar.Collapse id="navbar-dark-example">
                             <Nav >
                                 <NavDropdown
@@ -19,19 +19,19 @@ function Navigation() {
                                     title="Projects"
                                     menuVariant="dark"
                                 >
-                                    <NavDropdown.Item href="#action/3.1">MoodRing</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">
+                                    <NavDropdown.Item target='blank' href="#action/3.1">MoodRing</NavDropdown.Item>
+                                    <NavDropdown.Item target='blank' href="#action/3.2">
                                         LeftOvers
                                     </NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Garagio</NavDropdown.Item>
+                                    <NavDropdown.Item  target='blank' href="#action/3.3">Garagio</NavDropdown.Item>
                                     <NavDropdown.Divider />
-                                    <NavDropdown.Item href="https://github.com/Madilyncooper">
+                                    <NavDropdown.Item target='blank' href="https://github.com/Madilyncooper">
                                         GitHub
                                     </NavDropdown.Item>
                                 </NavDropdown>
                             </Nav>
                         </Navbar.Collapse>
-                        <Nav.Link href="#pricing">Resume</Nav.Link>
+                        <Nav.Link target='blank' href="https://docs.google.com/document/d/17hIFSzhCEs0dAwyi7bsUO2TpJRhqiTAGYA9ApXO62s8/edit?usp=sharing">Resume</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
