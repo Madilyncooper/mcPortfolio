c9cfe6501fcb3601a96f085784afefc734617453
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-09-10 15:45:19 -0700

    Added more navigation images and their styling. Linked google doc resume to resume in navigation. Added more styling for home page hover

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import AboutMe from '../assets/AboutMe.png';
 import ProjectIcon from '../assets/ProjectIcon.png';
+import ResumeIcon from '../assets/ResumeIcon.png';
+import ContactIcon from '../assets/ContactIcon.png';
 
 function Navigation() {
     return (
@@ -8,7 +10,9 @@ function Navigation() {
             <h1 className='explore'>Get <br></br>to Know Me</h1>
             <ul className='navList'>
                 <li>
-                    <Link className='aboutParent' to="/about">
+                    <Link
+                        className='aboutParent'
+                        to="/about">
                         <img
                             className="aboutImage"
                             src={AboutMe}
@@ -18,13 +22,37 @@ function Navigation() {
                     </Link>
                 </li>
                 <li>
-                    <Link className='projectParent' to="/projects">
-                    <img width="100" height="100" src="https://img.icons8.com/ios/100/FFFFFF/qr-code--v1.png" alt="qr-code--v1"/>
+                    <Link
+                        className='projectParent'
+                        to="/projects">
+                        <img
+                            width="100"
+                            height="100" src={ProjectIcon}
+                            alt="qr-code--v1" />
                         <p className="projects">Projects</p>
                     </Link>
                 </li>
-                <li><a className='resume'>Resume</a></li>
-                <li><a className='contact'>Contact</a></li>
+                <li>
+                    <Link
+                        className='resumeParent'
+                        to="https://docs.google.com/document/d/17hIFSzhCEs0dAwyi7bsUO2TpJRhqiTAGYA9ApXO62s8/edit?usp=sharing">
+                        <img
+                            width="100"
+                            height="100" src={ResumeIcon}
+                            alt="paperclip" />
+                        <p className="resume">Resume</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className='contactParent'
+                        to="https://docs.google.com/document/d/17hIFSzhCEs0dAwyi7bsUO2TpJRhqiTAGYA9ApXO62s8/edit?usp=sharing">
+                        <img
+                            src={ContactIcon}
+                            alt="paperclip" />
+                        <p className="contact">Contact</p>
+                    </Link>
+                </li>
             </ul>
         </div>
     );
