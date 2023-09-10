31fb132331530929c778e8ab57ee064b5ddf6c90
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-09-10 16:04:21 -0700

    Added target for a new tab when clicking resume link

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,21 +10,19 @@ function Navigation() {
             <h1 className='explore'>Get <br></br>to Know Me</h1>
             <ul className='navList'>
                 <li>
-                    <Link
-                        className='aboutParent'
-                        to="/about">
+                    <Link className='aboutParent' to="/about">
+                        
                         <img
                             className="aboutImage"
                             src={AboutMe}
                             alt="White icon of a head with cogs and gears inside">
                         </img>
                         <p className="about">AboutMe</p>
+
                     </Link>
                 </li>
                 <li>
-                    <Link
-                        className='projectParent'
-                        to="/projects">
+                    <Link className='projectParent' to="/projects">
                         <img
                             width="100"
                             height="100" src={ProjectIcon}
@@ -33,9 +31,7 @@ function Navigation() {
                     </Link>
                 </li>
                 <li>
-                    <Link
-                        className='resumeParent'
-                        to="https://docs.google.com/document/d/17hIFSzhCEs0dAwyi7bsUO2TpJRhqiTAGYA9ApXO62s8/edit?usp=sharing">
+                    <Link className='resumeParent' onClick={() => window.open("https://docs.google.com/document/d/17hIFSzhCEs0dAwyi7bsUO2TpJRhqiTAGYA9ApXO62s8/edit?usp=sharing", "_blank")}>
                         <img
                             width="100"
                             height="100" src={ResumeIcon}
@@ -44,9 +40,7 @@ function Navigation() {
                     </Link>
                 </li>
                 <li>
-                    <Link
-                        className='contactParent'
-                        to="https://docs.google.com/document/d/17hIFSzhCEs0dAwyi7bsUO2TpJRhqiTAGYA9ApXO62s8/edit?usp=sharing">
+                    <Link className='contactParent'>
                         <img
                             src={ContactIcon}
                             alt="paperclip" />
