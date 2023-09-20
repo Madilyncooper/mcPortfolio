94940ab8c0dd0bd2660ff5044360017095c44df1
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-09-19 18:14:55 -0700

    Added pictures to about page and styling/animation to them

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,36 @@
 import MadilynPic from '../assets/MadilynPic.png';
-// import PartnerPic from '../assets/PartnerPic.png';
-// import RockClimb from '../assets/RockClimb.png';
-// import SkatePic from '../assets/SkatePic.png';
-// import BeachPic from '../assets/BeachPic.png';
+import PartnerPic from '../assets/PartnerPic.png';
+import RockClimb from '../assets/RockClimb.png';
+import BeachPic from '../assets/BeachPic.png';
 
 function About() {
     return (
         <>
             <div className='picContainer'>
-                <img
-                    className="aboutImage"
-                    src={MadilynPic}
-                    alt="Picture of Madilyn Cooper">
+            <img
+                    className="rockPic"
+                    src={RockClimb}
+                    alt="Madilyn Cooper belaying someone at a rock climbing gym">
                 </img>
+                <img
+                    className="partnerPic"
+                    src={PartnerPic}
+                    alt="Madilyn Cooper and her life partner Monica">
+                </img>
+                <img
+                    className="beachPic"
+                    src={BeachPic}
+                    alt="Manhattan Beach pier overlooking the beach with rainbow lifeguard tower">
+                </img>
+                <img
+                    className="madilynPic"
+                    src={MadilynPic}
+                    alt="Portrait of Madilyn Cooper">
+                </img>
+            </div>
+            <div className='aboutInfoContainer'>
+                <h1 className='header'>About me</h1>
+<p className='page'>Hello about page Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam eos et suscipit, voluptas quas tempora quisquam quo sunt dolor omnis eaque fuga alias harum accusantium nisi optio veritatis facere in!</p>
             </div>
         </>
     )
