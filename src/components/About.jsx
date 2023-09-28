908f315dc78066d19e0682d721f2f53c21ba92a1
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-09-27 19:03:47 -0700

    Added bio info and styling to that

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,48 +29,49 @@ function About() {
                 </img>
             </div>
             <div className='skills'>
-            <p className='problemSolver fs-1'>Problem Solver.</p>
-            <p className='creative fs-1'>Creative.</p>
-            <p className='leader fs-1'>Leader.</p>
-            <p className='teamOriented fs-1'>Team Oriented.</p>
-            <p className='passionate fs-1'>Passionate.</p>
+                <p className='problemSolver fs-1'>Problem Solver.</p>
+                <p className='creative fs-1'>Creative.</p>
+                <p className='leader fs-1'>Leader.</p>
+                <p className='teamOriented fs-1'>Team Oriented.</p>
+                <p className='passionate fs-1'>Passionate.</p>
             </div>
             <div className='bio'>
                 <div className='who'>
-                <h1>
-                Who am I?
-                </h1>
-                <p>
-                Full Stack web developer who is passionate about solving problems and creating clean, 
-                effective, and well-structured applications.
-                I specialize in JavaScript, React.JS, Node, Restful APIs, and SQL, as well as many other 
-                back-end and front-end technologies.
-                </p>
-                </div>
-
-                <div className='why'>
-                <h1>
-                Why Tech?
-                </h1>
-                <p>
-                As a queer, black woman in tech, I take pride in bridging the gap between marginalized 
-                communities and the accessibility into this field. 
-                </p>
+                    <h1>
+                        Who am I?
+                    </h1>
+                    <p>
+                        Full Stack web developer who is passionate about solving problems and creating clean,
+                        effective, and well-structured applications.
+                        I specialize in JavaScript, React.JS, Node, Restful APIs, and SQL, as well as many other
+                        back-end and front-end technologies.
+                    </p>
                 </div>
 
                 <div className='what'>
-                <h1>
-                What do I bring to the table?
-                </h1>
-                <p>
-                My background in the arts has given me invaluable communication, emotional, and creative 
-                skills. These skills have helped shape my pursuit to be a kindhearted and thoughtful 
-                colleague, and lend themselves to my ability to constructively think outside the box .
-                I find joy in collaborating with others to produce the highest quality product, and I excel in 
-                team leadership roles. I am forever committed to growth, whether that be individually, or collectively
-                with my team.  
-                </p>
+                    <h1>
+                        What do I bring to the table?
+                    </h1>
+                    <p>
+                        My background in the arts has given me invaluable communication, emotional, and creative
+                        skills. These skills have helped shape my pursuit to be a kindhearted and thoughtful
+                        colleague, and lend themselves to my ability to constructively think outside the box .
+                        I find joy in collaborating with others to produce the highest quality product, and I excel in
+                        team leadership roles. I am forever committed to growth, whether that be individually, or collectively
+                        with my team.
+                    </p>
                 </div>
+
+                <div className='why'>
+                    <h1>
+                        Why Tech?
+                    </h1>
+                    <p>
+                        As a queer, black woman in tech, I take pride in bridging the gap between marginalized
+                        communities and the accessibility into this field. I have been 
+                    </p>
+                </div>
+
             </div>
         </>
     )
